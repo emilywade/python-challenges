908f42620921c3438304d47591025ae0c73b5ec9
emilywade <57637958+emilywade@users.noreply.github.com>
2020-03-21 12:54:09 +0000

    Add files via upload

---
+++ User+input+to+number.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[1]:
+
+int_num = int(input('Give me an integer number: '))
+float_num = float(input('Give me a float  number: '))
+result = int_num * float_num
+print('Your result is: ', result)
+
+
+# In[2]:
+
+int_text = input("Give me an integer number: ")
+int_num = int(int_text)
+float_text = input("Give me a float  number: ")
+float_num = float(float_text)
+result = int_num * float_num
+print("Your result is: ", result)
+
+
+# In[ ]:
+
+
+
